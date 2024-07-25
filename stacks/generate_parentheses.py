5b8d1eec5b829d2578c2b2fd59100251b692ee27
Sandro <sbuzhghulashvili@gmail.com>
2024-07-26 02:10:27 +0400

    added js solution

--- stacks/generate_parentheses.py
+++ stacks/generate_parentheses.py
@@ -1,3 +1,33 @@
+# my javascript solution
+
+# var generateParenthesis = function(n) {
+#     const result = []
+#     const stack = []
+
+#     function backtrack(openCount, closeCount) {
+#         if(openCount == n && closeCount === n) {
+#             result.push(stack.join(""))
+#             return 
+#         }
+#         if(openCount < n) {
+#             stack.push("(")
+#             backtrack(openCount + 1, closeCount)
+#             stack.pop()
+#         }
+#         if(closeCount < openCount) {
+#             stack.push(")")
+#             backtrack(openCount, closeCount + 1)
+#             stack.pop()
+#         }
+#     }
+
+#     backtrack(0, 0)
+#     return result
+# };
+
+
+# neetcode python solution
+
 def generateParenthesis(n):
     # only add open parenthesis if open < n
     # only add a closing parenthesis if closed < open
