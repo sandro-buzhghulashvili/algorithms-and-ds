470595f19ad2849e215fb42bfacbdec20f81d50b
Sandro <sbuzhghulashvili@gmail.com>
2024-07-25 22:40:18 +0400

    generate parenthesis

---
+++ stacks/generate_parentheses.py
@@ -0,0 +1,27 @@
+def generateParenthesis(n):
+    # only add open parenthesis if open < n
+    # only add a closing parenthesis if closed < open
+    # valid if open == closed == n
+    stack = []
+    res = []
+
+    def backtrack(openCount, closedCount):
+        if openCount == closedCount == n:
+            res.append("".join(stack))
+            return 
+        if openCount < n:
+            stack.append("(")
+            backtrack(openCount + 1, 0)
+            stack.pop()
+        if closedCount < openCount:
+            stack.append(")")
+            backtrack(openCount, closedCount + 1)
+            stack.pop()
+
+    backtrack(0, 0)
+    print(res)
+
+generateParenthesis(3)
+
+
+## res = ["((()))"]
